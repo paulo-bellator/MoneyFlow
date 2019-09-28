e0ce634dbc0d7bb0ca024d86e1816f301cf6b2b1
paulo-bellator <nikita.goncharov81@gmail.com>
2019-09-29 00:47:50 +0300

    add VisionTextRecognizer to parse ops from screens

--- MoneyFlow/Graph Chart/TestViewController.swift
+++ MoneyFlow/Graph Chart/TestViewController.swift
@@ -7,64 +7,156 @@
 //
 
 import UIKit
+import Firebase
 
-class TestViewController: UIViewController, ChartViewDelegate {
-    func chartView(didSelectColumnAt index: Int) {
-        print("Selected in \(index)")
-    }
+class TestViewController: UIViewController {
     
-    func chartViewNumberOfColumns() -> Int {
-        return 12
-    }
     
-    func chartView(labelForColumnAt index: Int) -> String {
-        return "Dec"
-    }
+    private lazy var recognizer = OperationVisionRecognizer()
     
-    func chartView(mainValueForColumnAt index: Int) -> CGFloat {
-        return CGFloat(Double.random(in: 0.1...0.8))
-    }
-    
-    func chartView(secondValueForColumnAt index: Int) -> CGFloat? {
-        return CGFloat(Double.random(in: 0.4...1.0))
-    }
-    
-
-    @IBOutlet weak var chartView: ChartView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        chartView.delegate = self
-        chartView.minValueLabel.text = "0"
-        chartView.midValueLabel.text = "50K"
-        self.chartView.measureLinesColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1).withAlphaComponent(0.5)
-        chartView.allowsSelection = true
-//        chartView.updateUI()
-//
-//        Timer.scheduledTimer(withTimeInterval: 4.2, repeats: false) { _ in
-//            self.chartView.bounds.size.height -= 200
-////            self.chartView.layoutIfNeeded()
-////            self.chartView.collectionView.reloadData()
-////            self.chartView.layoutIfNeeded()
-//        }
-//        Timer.scheduledTimer(withTimeInterval: 6.0, repeats: false) { _ in
-////            self.chartView.layoutIfNeeded()
-//            print("height: \(self.chartView.bounds.height)")
-//        }
         
-//        Timer.scheduledTimer(withTimeInterval: 4.3, repeats: false) { _ in
-//            self.chartView.updateUI()
-//        }
-//        chartView.updateLayoutsOfSubviews()
-        
-        
-        
-        // Do any additional setup after loading the view.
-    }
-    override var preferredStatusBarStyle: UIStatusBarStyle {
-        return .lightContent
+        let image = UIImage(named: "sberbank_ops_screen")!
+        let _ = recognizer.recognize(from: image)
     }
     
-
-
 }
+
+class OperationVisionRecognizer {
+    
+    private var recognizer: VisionTextRecognizer!
+    private lazy var vision = Vision.vision()
+    
+    
+    func recognize(from image: UIImage) -> (operations: [Operation]?, error: Error?) {
+        var ops = [Operation]()
+        var resultError: Error?
+        var result: VisionText?
+        let visionImage = VisionImage(image: image)
+        
+        recognizer.process(visionImage) { recognizerData, error in
+          guard error == nil, let data = recognizerData else {
+            resultError = error
+            return
+          }
+            result = data
+            ops = self.sberbankParser(from: data)
+        }
+        
+        if let error = resultError {
+            return (nil, error)
+        } else {
+//            ops = sberbankParser(from: result!)
+        }
+        
+        return (ops, resultError)
+    }
+    
+    private func sberbankParser(from visionText: VisionText) -> [Operation]  {
+        var ops = [Operation]()
+        
+        var lines = [[VisionTextBlock]]()
+        
+        for block in visionText.blocks {
+            if lines.isEmpty { lines.append([block]); continue }
+            for (index, line) in lines.enumerated() {
+                if (abs(line[0].frame.minY - block.frame.minY) < 50.0
+                    || abs(line[0].frame.minY - block.frame.maxY) < 30.0
+                    || abs(line[0].frame.maxY - block.frame.minY) < 30.0) {
+                    lines[index].append(block)
+                } else if index == lines.count - 1 {
+                    lines.append([block])
+                }
+            }
+        }
+        
+        for (index, line) in lines.enumerated() {
+            lines[index] = line.sorted { $0.frame.minX < $1.frame.minX }
+        }
+        
+        for line in lines {
+            var text = ""
+            for block in line {
+                let string = block.text.replacingOccurrences(of: "\n", with: " ")
+                text += string + " | "
+            }
+            text.removeLast()
+            print(text)
+        }
+        
+        for line in lines {
+            if let date = dateFromSberbank(line[0].text) {
+                print(date.formattedDescription)
+            }
+            if let value = valueFromSberbank(line.last!.text) {
+                print(value.currencyFormattedDescription(.rub))
+            }
+        }
+        
+        var rawOps = [ (Date, String, String, Double) ]()
+        
+        var currentDate: Date?
+        for (index, var line) in lines.enumerated() {
+            if let date = dateFromSberbank(line[0].text) {
+                currentDate = date
+                continue
+            }
+            if let value = valueFromSberbank(line.last!.text), let date = currentDate {
+                line.removeLast()
+                if line.first!.text.count < 3 { line.removeFirst() }
+                let subject = line.map({$0.text}).joined(separator: " ").replacingOccurrences(of: "\n", with: " ")
+                var category = ""
+                if index+1 < lines.count {
+                    if lines[index+1].count < 3 { category = lines[index+1].first!.text }
+                }
+                rawOps.append((date, category, subject, value))
+            }
+        }
+        
+        print("\n")
+        rawOps.forEach { print($0) }
+        
+        return ops
+    }
+    
+    func valueFromSberbank(_ string: String) -> Double? {
+        guard string.contains("Р") || string.contains("P") else {
+            return nil
+        }
+        
+        let sign = string.contains("+") ? "" : "-"
+        let numericString = string.filter { "0123456789,".contains($0) }
+        
+        var valueString = numericString
+        if valueString.count > 3 {
+            let range = valueString.index(valueString.endIndex, offsetBy: -3)..<valueString.endIndex
+            valueString = valueString.replacingOccurrences(of: ",", with: ".", options: [], range: range).filter { !",".contains($0) }
+        }
+        let resultString = sign + valueString
+        return Double(resultString)
+    }
+    
+    private func dateFromSberbank(_ string: String) -> Date? {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ru_RU")
+        formatter.dateFormat = "dd MMMM, EE"
+        
+        if let date = formatter.date(from: string) {
+            let calendar = Calendar.current
+            let day = calendar.component(.day, from: date)
+            let month = calendar.component(.month, from: date)
+            let year = calendar.component(.year, from: Date())
+            return calendar.date(from: DateComponents(year: year, month: month, day: day, hour: 12))
+           
+        } else { return nil }
+    }
+    
+    init() {
+        let options = VisionCloudTextRecognizerOptions()
+        options.languageHints = ["en", "ru"]
+//        recognizer = vision.onDeviceTextRecognizer()
+        recognizer = vision.cloudTextRecognizer(options: options)
+    }
+}
+
