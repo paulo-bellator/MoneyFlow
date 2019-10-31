3fa5fb507a131f23abfd047ccc2c6946fd151cb8
paulo-bellator <nikita.goncharov81@gmail.com>
2019-10-31 22:35:14 +0300

    add animation

--- MoneyFlow/Screens/Authorization/StartViewController.swift
+++ MoneyFlow/Screens/Authorization/StartViewController.swift
@@ -18,6 +18,7 @@ class StartViewController: UIViewController {
     private let tabBarSegueIdentifier = "tabBarSegue"
     private let context = LAContext()
     private var canEvaluatePolicy = false
+    fileprivate var timer: Timer?
     
     
     @IBAction func logInButtonTouched(_ sender: UIButton) {
@@ -51,6 +52,16 @@ class StartViewController: UIViewController {
         }
     }
     
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        timer?.invalidate()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        currencySignsAppearingAnimation()
+    }
+    
     private func logIn() {
         let biometricService = context.biometryType == .faceID ? "FaceID" : "Touch ID"
         if canEvaluatePolicy {
@@ -82,7 +93,98 @@ class StartViewController: UIViewController {
             tabBarVC.selectedIndex = 1
         }
     }
-    
+}
 
-   
+extension StartViewController {
+    private struct Constants {
+        static let currencySigns = ["$","€","¥","£","₽"]
+        static let colors: [UIColor] = [#colorLiteral(red: 0.7725490196, green: 0.8784313725, blue: 0.7058823529, alpha: 1), #colorLiteral(red: 0.4, green: 0.462745098, blue: 0.9490196078, alpha: 1), #colorLiteral(red: 0.9568627451, green: 0.6941176471, blue: 0.5137254902, alpha: 1)]
+        static let font = UIFont(name: "CenturyGothic-Bold", size: 18.0)
+        static let fontSizes: [CGFloat] = [12.0, 14.0, 16.0, 18.0 , 20.0, 22.0, 24.0]
+        static let minRotationAngle: CGFloat = -CGFloat.pi / 4
+        static let maxRotationAngle: CGFloat = CGFloat.pi / 4
+        static let borderSpacing: CGFloat = 20.0
+    }
+    
+    func currencySignsAppearingAnimation() {
+        currencySignAppear()
+        for step in 1...7 {
+            Timer.scheduledTimer(withTimeInterval: 0.15 * Double(step), repeats: false) { [weak self] _ in
+                self?.currencySignAppear()
+            }
+        }
+        timer = Timer.scheduledTimer(withTimeInterval: 3.5, repeats: true) { [weak self] _ in
+            self?.currencySignAppear()
+            for step in 1...7 {
+                Timer.scheduledTimer(withTimeInterval: 0.15 * Double(step), repeats: false) { [weak self] _ in
+                    self?.currencySignAppear()
+                }
+            }
+        }
+    }
+    
+    private func currencySignAppear() {
+        let label = UILabel()
+        label.text = Constants.currencySigns.randomElement()!
+        label.sizeToFit()
+        label.textColor = Constants.colors.randomElement()!
+        label.font = Constants.font?.withSize(Constants.fontSizes.randomElement()!)
+        label.frame = randomFrame(for: label)
+        let rotationAngle = CGFloat.random(in: Constants.minRotationAngle...Constants.maxRotationAngle)
+        label.transform = CGAffineTransform.init(rotationAngle: rotationAngle)
+        label.alpha = 0.0
+        self.view.addSubview(label)
+        
+        UIView.animate(
+            withDuration: 0.5,
+            animations: { label.alpha = 1.0 },
+            completion: { _ in
+                Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
+                    UIView.animate(
+                        withDuration: 0.5,
+                        animations: { label.alpha = 0.0 },
+                        completion: { _ in label.removeFromSuperview() })
+                }
+            })
+    }
+    
+    private func randomFrame(for label: UILabel) -> CGRect {
+        let width = label.bounds.width * 2
+        let height = label.bounds.height * 2
+        
+        let minX = Constants.borderSpacing
+        let maxX = view.bounds.maxX - Constants.borderSpacing - width
+        let minY = Constants.borderSpacing
+        let maxY = view.bounds.maxY - Constants.borderSpacing - height
+        
+        var originX = minX
+        var originY = minY
+        var resultFrame = CGRect.zero
+        
+        var areCoordinatesCorrect = false
+        var counter = 0
+        let maxCount = 50
+        
+        while !areCoordinatesCorrect {
+            counter += 1
+            originX = CGFloat.random(in: minX...maxX)
+            originY = CGFloat.random(in: minY...maxY)
+            resultFrame = CGRect(x: originX, y: originY, width: width, height: height)
+            areCoordinatesCorrect = true
+            
+            for subview in view.subviews {
+                if subview.frame.intersects(resultFrame) {
+                    areCoordinatesCorrect = false
+                    break
+                }
+            }
+            
+            if counter == maxCount {
+                resultFrame = CGRect.zero
+                break
+            }
+        }
+        
+        return resultFrame
+    }
 }
